a37f5da46b2623dd1e612c0b77377ccfeab4deac
Madoc <32127185+madocb@users.noreply.github.com>
2017-09-27 11:59:30 +0100

    Update ip2as.py

--- ip2as.py
+++ ip2as.py
@@ -1,17 +1,27 @@
+#!/usr/bin/python
+#
+#IP2AS.py
 #Script to Get AS number from IP address
-#In Development
 
 import requests
 
-prefix = input("Enter IP Prefix:")
+hit=0
+
+prefix = input("Enter IP Prefix: ")
 print ("looking for:",prefix)
 
 res = requests.get("http://ftp.ripe.net/ripe/stats/delegated-ripencc-latest")
 
 mad = res.text
-print(mad.find(prefix))
+#print(mad.find(prefix))
 
 for item in mad.split("\n"):
     if "|"+prefix in item:
         print (item.strip())
+        matchline = (item.strip())
+        print ("Input:",prefix,"Prefix",matchline.split("|")[3],"AS Number: AS",matchline.split("|")[4])
+        hit += 1
         break
+
+if hit == 0:
+    print ("Sorry no match for ",prefix)
